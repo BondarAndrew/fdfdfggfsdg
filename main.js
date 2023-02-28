85e8b0a5272cdfb02b71c28ebe44b84138a1cf44
BondarAndrew <107639998+BondarAndrew@users.noreply.github.com>
2023-02-28 21:10:40 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+//ex1
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(numbers.length);
+
+//ex2
+
+let names = [
+  "Andrew",
+  "Max",
+  "Oleg",
+  "Platon",
+  "Artem",
+  "Stas",
+  "Ivan",
+  "Alex",
+  "Vova",
+  "Nikita",
+];
+console.log(names[0]);
+console.log(names[5]);
+console.log(names[9]);
+
+//ex3
+
+let data = [];
+console.log(data);
+data[0] = 10;
+data[1] = 8;
+data[9] = 100;
+console.log(data);
